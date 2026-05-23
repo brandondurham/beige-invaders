40b2282e5c3fa16ba2da34815dec2be661b5b7e4
bdurham-simpleclosure <bdurham@simpleclosure.com>
2026-05-23 14:20:56 -0700

    bandw

--- app/space/consts.ts
+++ app/space/consts.ts
@@ -1,86 +1,39 @@
+type RGB = [number, number, number];
 
-export const COLOR_WHITE = [255, 255, 255] as [number, number, number];
+interface Theme {
+  HAS_SHADOW: boolean;
+  COLOR_CANVAS_BG: RGB;
+  COLOR_H1: RGB;
+  COLOR_PLAYER: RGB;
+  COLOR_PLAYER_ACTIVE: RGB;
+  COLOR_PLAYER_BULLET: RGB;
+  COLOR_GAMEOVER_HEADING: RGB;
+  COLOR_UI_FONT: RGB;
+  COLOR_LIVES: RGB;
+  COLOR_ACCENT: RGB;
+  COLOR_DIM: RGB;
+  COLOR_ENEMY: RGB;
+  COLOR_ENEMY_ROWS: RGB[];
+  COLOR_ENEMY_BULLET: RGB;
+  COLOR_EXPLOSION: RGB;
+  COLOR_FRAME: RGB;
+  COLOR_HI_SCORE: RGB;
+  COLOR_SHADOW: RGB;
+  COLOR_SHIELD: RGB;
+  COLOR_UFO: RGB;
+  DIALOG_BG: RGB;
+  DIALOG_FG: RGB;
+  DIALOG_BTN_YES_BG: RGB;
+  DIALOG_BTN_YES_FG: RGB;
+  DIALOG_BTN_NO_BG: RGB;
+  DIALOG_BTN_NO_FG: RGB;
+  COLOR_TOUCH_BTN_BG: string;
+  COLOR_TOUCH_BTN_BORDER: string;
+  COLOR_TOUCH_BTN_TEXT: string;
+  SPLAT_COLORS: RGB[];
+}
 
-// COLOR: B&W
-// export const HAS_SHADOW = false;
-// const COLOR_BASE = [0, 0, 0] as [number, number, number];
-// export const COLOR_CANVAS_BG = [212, 212, 212] as [number, number, number];
-// export const COLOR_H1 = COLOR_BASE;
-// export const COLOR_PLAYER = [30, 105, 250] as [number, number, number];
-// export const COLOR_GAMEOVER_HEADING = COLOR_BASE;
-// export const COLOR_UI_FONT = COLOR_BASE;
-// export const COLOR_EXPLOSION = COLOR_BASE;
-// export const COLOR_PLAYER_ACTIVE = [255, 0, 185] as [number, number, number];
-// export const COLOR_ACCENT = [0, 255, 65] as [number, number, number];
-// export const COLOR_DIM = [100, 100, 100] as [number, number, number];
-// export const COLOR_ENEMY = COLOR_BASE;
-// export const COLOR_ENEMY_ROWS: [number, number, number][] = [
-//   [251, 112, 201],
-//   [82, 191, 179],
-//   [247, 215, 90],
-//   [194, 153, 237],
-//   [58, 127, 237],
-// ];
-// export const COLOR_ENEMY_BULLET = COLOR_BASE;
-// export const COLOR_FRAME = COLOR_BASE;
-// export const COLOR_HI_SCORE = [255, 235, 59] as [number, number, number];
-// export const COLOR_PLAYER_BULLET = COLOR_PLAYER;
-// export const COLOR_SHADOW = [0, 0, 0] as [number, number, number];
-// export const COLOR_SHIELD = [237, 203, 188] as [number, number, number];
-// export const COLOR_UFO = COLOR_BASE;
-// export const COLOR_LIVES = COLOR_UI_FONT;
-// export const DIALOG_BG = [20, 20, 20] as [number, number, number];
-// export const DIALOG_FG = [236, 236, 236] as [number, number, number];
-// export const DIALOG_BTN_YES_BG = [30, 100, 255] as [number, number, number];
-// export const DIALOG_BTN_YES_FG = COLOR_WHITE;
-// export const DIALOG_BTN_NO_BG = COLOR_BASE;
-// export const DIALOG_BTN_NO_FG = COLOR_WHITE;
-// export const COLOR_TOUCH_BTN_BG = 'rgba(255,255,255,0.08)';
-// export const COLOR_TOUCH_BTN_BORDER = 'rgba(255,255,255,0.35)';
-// export const COLOR_TOUCH_BTN_TEXT = 'rgba(255,255,255,0.7)';
-// export const SPLAT_COLORS: [number, number, number][] = [
-//   [255, 0, 30], [255, 0, 185], [255, 247, 0], [12, 36, 255], [0, 105, 12],
-//   [255, 134, 243], [0, 255, 165], [0, 221, 255], [111, 0, 159],
-// ];
-
-// COLOR: LIMBO
-export const HAS_SHADOW = true;
-const COLOR_BASE = [35, 33, 30] as [number, number, number];
-export const COLOR_CANVAS_BG = [18, 16, 14] as [number, number, number];
-export const COLOR_H1 = [225, 222, 215] as [number, number, number];
-export const COLOR_PLAYER = [252, 250, 244] as [number, number, number];
-export const COLOR_GAMEOVER_HEADING = [225, 222, 215] as [number, number, number];
-export const COLOR_UI_FONT = [175, 172, 165] as [number, number, number];
-export const COLOR_EXPLOSION = [140, 138, 132] as [number, number, number];
-export const COLOR_PLAYER_ACTIVE = [255, 255, 255] as [number, number, number];
-export const COLOR_ACCENT = [252, 250, 244] as [number, number, number];
-export const COLOR_DIM = [65, 63, 60] as [number, number, number];
-export const COLOR_ENEMY = [100, 98, 93] as [number, number, number];
-export const COLOR_ENEMY_ROWS: [number, number, number][] = [
-  [85, 83, 80],
-  [115, 113, 108],
-  [145, 143, 137],
-  [175, 172, 165],
-  [205, 202, 195],
-];
-export const COLOR_ENEMY_BULLET = [195, 192, 186] as [number, number, number];
-export const COLOR_FRAME = [55, 53, 50] as [number, number, number];
-export const COLOR_HI_SCORE = [225, 222, 215] as [number, number, number];
-export const COLOR_PLAYER_BULLET = COLOR_PLAYER;
-export const COLOR_SHADOW = [8, 8, 6] as [number, number, number];
-export const COLOR_SHIELD = [75, 73, 70] as [number, number, number];
-export const COLOR_UFO = [235, 233, 228] as [number, number, number];
-export const COLOR_LIVES = COLOR_UI_FONT;
-export const DIALOG_BG = [10, 10, 8] as [number, number, number];
-export const DIALOG_FG = [210, 208, 202] as [number, number, number];
-export const DIALOG_BTN_YES_BG = [225, 222, 215] as [number, number, number];
-export const DIALOG_BTN_YES_FG = [10, 10, 8] as [number, number, number];
-export const DIALOG_BTN_NO_BG = COLOR_BASE;
-export const DIALOG_BTN_NO_FG = [175, 172, 165] as [number, number, number];
-export const COLOR_TOUCH_BTN_BG = 'rgba(200,198,192,0.08)';
-export const COLOR_TOUCH_BTN_BORDER = 'rgba(200,198,192,0.35)';
-export const COLOR_TOUCH_BTN_TEXT = 'rgba(200,198,192,0.7)';
-export const SPLAT_COLORS: [number, number, number][] = [
+const SPLAT_VIVID: RGB[] = [
   [255, 0, 30],
   [255, 0, 185],
   [255, 247, 0],
@@ -91,6 +44,88 @@ export const SPLAT_COLORS: [number, number, number][] = [
   [0, 221, 255],
   [111, 0, 159],
 ];
+
+const themes = {
+
+  bandw: {
+    HAS_SHADOW: true,
+    COLOR_CANVAS_BG:        [18, 16, 14] as RGB,
+    COLOR_H1:               [225, 222, 215] as RGB,
+    COLOR_PLAYER:           [252, 250, 244] as RGB,
+    COLOR_PLAYER_ACTIVE:    [255, 255, 255] as RGB,
+    COLOR_PLAYER_BULLET:    [252, 250, 244] as RGB,
+    COLOR_GAMEOVER_HEADING: [225, 222, 215] as RGB,
+    COLOR_UI_FONT:          [175, 172, 165] as RGB,
+    COLOR_LIVES:            [175, 172, 165] as RGB,
+    COLOR_ACCENT:           [252, 250, 244] as RGB,
+    COLOR_DIM:              [65, 63, 60] as RGB,
+    COLOR_ENEMY:            [100, 98, 93] as RGB,
+    COLOR_ENEMY_ROWS: [
+      [85, 83, 80],
+      [115, 113, 108],
+      [145, 143, 137],
+      [175, 172, 165],
+      [205, 202, 195],
+    ] as RGB[],
+    COLOR_ENEMY_BULLET:     [195, 192, 186] as RGB,
+    COLOR_EXPLOSION:        [140, 138, 132] as RGB,
+    COLOR_FRAME:            [55, 53, 50] as RGB,
+    COLOR_HI_SCORE:         [225, 222, 215] as RGB,
+    COLOR_SHADOW:           [8, 8, 6] as RGB,
+    COLOR_SHIELD:           [75, 73, 70] as RGB,
+    COLOR_UFO:              [235, 233, 228] as RGB,
+    DIALOG_BG:              [10, 10, 8] as RGB,
+    DIALOG_FG:              [210, 208, 202] as RGB,
+    DIALOG_BTN_YES_BG:      [225, 222, 215] as RGB,
+    DIALOG_BTN_YES_FG:      [10, 10, 8] as RGB,
+    DIALOG_BTN_NO_BG:       [35, 33, 30] as RGB,
+    DIALOG_BTN_NO_FG:       [175, 172, 165] as RGB,
+    COLOR_TOUCH_BTN_BG:     'rgba(200,198,192,0.08)',
+    COLOR_TOUCH_BTN_BORDER: 'rgba(200,198,192,0.35)',
+    COLOR_TOUCH_BTN_TEXT:   'rgba(200,198,192,0.7)',
+    SPLAT_COLORS: SPLAT_VIVID,
+  },
+
+} satisfies Record<string, Theme>;
+
+export type ThemeName = keyof typeof themes;
+export const ACTIVE_THEME: ThemeName = 'bandw';
+
+const t = themes[ACTIVE_THEME];
+
+export const COLOR_WHITE: RGB = [255, 255, 255];
+export const {
+  HAS_SHADOW,
+  COLOR_CANVAS_BG,
+  COLOR_H1,
+  COLOR_PLAYER,
+  COLOR_PLAYER_ACTIVE,
+  COLOR_PLAYER_BULLET,
+  COLOR_GAMEOVER_HEADING,
+  COLOR_UI_FONT,
+  COLOR_LIVES,
+  COLOR_ACCENT,
+  COLOR_DIM,
+  COLOR_ENEMY,
+  COLOR_ENEMY_ROWS,
+  COLOR_ENEMY_BULLET,
+  COLOR_EXPLOSION,
+  COLOR_FRAME,
+  COLOR_HI_SCORE,
+  COLOR_SHADOW,
+  COLOR_SHIELD,
+  COLOR_UFO,
+  DIALOG_BG,
+  DIALOG_FG,
+  DIALOG_BTN_YES_BG,
+  DIALOG_BTN_YES_FG,
+  DIALOG_BTN_NO_BG,
+  DIALOG_BTN_NO_FG,
+  COLOR_TOUCH_BTN_BG,
+  COLOR_TOUCH_BTN_BORDER,
+  COLOR_TOUCH_BTN_TEXT,
+  SPLAT_COLORS,
+} = t;
 
 export const NUM_COLORS_IN_SPLAT = 5;
 export const ENEMY_SPEED_INCREASE_PER_LEVEL = 0.18;
